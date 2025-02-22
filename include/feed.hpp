9ed17f9e78fd09a7e0df5aa02e7c21f6e4bb2b14
Ian Andwati <andwatiian@gmail.com>
2025-02-23 00:37:27 +0300

    feed settings struct

--- include/feed.hpp
+++ include/feed.hpp
@@ -2,19 +2,45 @@
 // Created by ian on 2/22/25.
 //
 #pragma once
+#include "feed.hpp"
+
 #include <QString>
 #include <vector>
+#include <QDateTime>
+#include <QUuid>
 
 struct FeedItem {
   QString title;
   QString link;
   QString description;
   QString pubDate;
+  QDateTime published;
+  bool isRead{false};
+  QUuid id{QUuid::createUuid()};
 };
 
 class RSSFeed {
 public:
   QString feedUrl;
   QString feedTitle;
+  QString description;
+  QString category;
   std::vector<FeedItem> items;
+  bool autoRefresh{true};
+  int refreshInterval{30}; // in minutes
+  QUuid id{QUuid::createUuid()};
+
+  // format settings
+  struct FormatSettings {
+    QString titleFont{"Arial"};
+    int titleSize{16};
+    QString contentFont{"Arial"};
+    int contentSize{12};
+    QString backgroundColor{"#FFFFFF"};
+    QString textColor{"#000000"};
+    QString linkColor{"#0066CC"};
+    bool showImages{true};
+    bool useCustomCSS{false};
+    QString customCSS;
+  } formatting;
 };
